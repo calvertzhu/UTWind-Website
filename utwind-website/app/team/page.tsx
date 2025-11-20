07d7c825237b55afe5bb3e9ab22d89669ca61b66
moose16 <matthewting16@gmail.com>
2025-11-19 19:13:34 -0500

    a

---
+++ utwind-website/app/team/page.tsx
@@ -0,0 +1,7 @@
+export default function TeamTest() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-200">
+      <h1 className="text-4xl font-bold">TEAM PAGE WORKS ✔️</h1>
+    </div>
+  );
+}
